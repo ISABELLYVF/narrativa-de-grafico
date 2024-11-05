aba0264107c3ac31202a82868e11c686e8946dc4
ISABELLYVF <126249501+ISABELLYVF@users.noreply.github.com>
2024-11-05 11:29:55 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,7 +1,7 @@
-import { getCSS, tickConfig, criarGrafico } from "./common.js"
+import { getCSS } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/ISABELLYVF/api/main/numero-usuarios.json'
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
@@ -20,38 +20,14 @@ async function quantidadeUsuariosPorRede() {
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
-        title: {
-            text: 'Redes sociais com mais usuários no mundo',
-            x: 0,
-            font: {
-                color: getCSS('--primary-color'),
-                family: getCSS('--font'),
-                size: 30
-            }
-        },
-        xaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'nome das redes sociais',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'bilhões de usuários ativos',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        }
+        paper_bgcolor: getCSS('--bg-color')
 
     }
 
-    criarGrafico(data, layout)
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuariosPorRede()
